98618716592ea5ec693a1192e1b1a040533f90ec
CarolusWildan <230712338@students.uajy.ac.id>
2026-06-19 15:02:46 +0700

    Update invoice screen

--- lib/features/history/presentation/screens/invoice_screen.dart
+++ lib/features/history/presentation/screens/invoice_screen.dart
@@ -1,10 +1,11 @@
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void downloadPdf(String bookingId) async {
   print('==== MENGUNDUH PDF UNTUK ID: $bookingId ====');
-  // 1. Ganti ngrok-nya dengan ngrok Anda yang aktif
-  // 2. Perhatikan penambahan '/api/bookings/'
-  final Uri url = Uri.parse('https://mortality-emote-creasing.ngrok-free.dev/api/bookings/$bookingId/download-pdf');
+
+  final String baseUrl = dotenv.env['API_BASE_URL'] ?? 'http://127.0.0.1:8000';
+  final Uri url = Uri.parse('$baseUrl/api/bookings/$bookingId/download-pdf');
   
   if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
     throw Exception('Gagal membuka $url');
